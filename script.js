05f70fa0260fd927e6a336170af8044678a219d3
Antonella Domenez <domenezantonellaa@gmail.com>
2024-07-05 16:59:45 +1000

    Subiendo mi proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirectToBooking() {
+  window.location.href = "https://fisioteam.square.site/s/appointments";
+}
+function redirectToGift() {
+  window.location.href = "https://fisioteam.square.site/s/gift-cards";
+}
