e9bf1e329066a91b90419f831d0e89fd9824c399
Antonella Domenez <domenezantonellaa@gmail.com>
2024-07-06 14:41:11 +1000

    agregando cambios de animacion

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function redirectToBooking() {
 function redirectToGift() {
   window.location.href = "https://fisioteam.square.site/s/gift-cards";
 }
+document.addEventListener("scroll", function () {
+  var elements = document.querySelectorAll(
+    ".slide-in-left, .slide-in-right, .slide-in-below, .slide-in-left-script,  .slide-in-right-script"
+  );
+
+  elements.forEach(function (element) {
+    var position = element.getBoundingClientRect();
+
+    // Verifica si el elemento está visible en la ventana del navegador
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+      element.classList.add("active");
+    }
+  });
+});
